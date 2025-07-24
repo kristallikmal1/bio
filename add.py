ee191b2499b3fc32482bbac47d7c47a99efb25ff
kristallikmal1 <mmytarev3@gmail.com>
2025-07-25 02:20:41 +0300

    dd.py

--- add.py
+++ add.py
@@ -1,6 +1,7 @@
 # app.py
 from flask import Flask, render_template
 import data
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def index():
     return render_template('index.html', data=data.bio_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
